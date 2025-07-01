0d4ec5cba3a80bc2aa6d29253c63dc13808588b8
Nyakundi <henrymoire@gmail.com>
2025-07-01 17:08:00 +0300

    Completed registration form
    The registration page now successfully connects to the database.

--- smdas/app/routes/login.tsx
+++ smdas/app/routes/login.tsx
@@ -46,7 +46,7 @@ export async function action({ request }: Route.ActionArgs) {
 
     return { error: "An unknown error occurred" };
   }
-
+  
   throw response;
 }
 
--- smdas/app/routes/register.tsx
+++ smdas/app/routes/register.tsx
@@ -1,5 +1,44 @@
-import { Outlet } from "react-router";
+import { Form, redirect, type ActionFunctionArgs } from "react-router";
+import { supabase } from "~/supabase-client";
+
+export async function action({request}: ActionFunctionArgs) {
+    const formData = await request.formData();
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const name = formData.get("name") as string;
+    
+    if(!email || !password || !name) {
+        return {error: "Missing data"};
+    }
+
+    const {data, error} = await supabase.from("Users").insert({name, password, email});
+
+    if(error) {
+        return {error: error.message};
+    }
+
+    return redirect("/login");
+}
 
 export default function Register() {
-    return <div>Welome to the dashboard!<Outlet /></div>
-};
+    return (
+        <div>
+            <h2>Register a new account</h2>
+            <Form method = "post">
+                <div>
+                    <label> Email </label>
+                    <input type="text" name="email" required/>
+                </div>
+                <div>
+                    <label> password </label>
+                    <input type="text" name="password" required/>
+                </div>
+                <div>
+                    <label> Username </label>
+                    <input type="text" name="name" required/>
+                </div>
+                <button type="submit">Create account</button>
+            </Form>
+        </div>
+    );
+}
