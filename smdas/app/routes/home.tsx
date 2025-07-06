8f37091763baf794ff5ae6c9f16a7376a1ff355f
Nyakundi <henrymoire@gmail.com>
2025-07-06 23:47:55 +0300

    Tentative final
    Everything works, just barely.

--- smdas/app/routes/home.tsx
+++ smdas/app/routes/home.tsx
@@ -15,7 +15,7 @@ export async function loader({ request }: Route.LoaderArgs) {
   // Check if the user is already logged in
     const userId = await getUserId(request);
     if (!userId) {
-      //throw redirect("/login");
+      throw redirect("/login");
     } else {
       return { userId };
     }
